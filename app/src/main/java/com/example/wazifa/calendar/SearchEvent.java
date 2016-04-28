e9cb4303a09b21a93737ff992a5f475ece572f5c
eeriquelme <eeriquelme@yahoo.com>
2016-04-28 00:57:05 -0500

    Search done

--- app/src/main/java/com/example/wazifa/calendar/SearchEvent.java
+++ app/src/main/java/com/example/wazifa/calendar/SearchEvent.java
@@ -1,14 +1,17 @@
 package com.example.wazifa.calendar;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -17,42 +20,53 @@ import android.widget.TextView;
 import com.firebase.client.Firebase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class SearchEvent extends Activity {
-    private ListView elist;
-    private User usr;
-    private DBmanager database;
-    private List<Event> events;
+public class SearchEvent extends AppCompatActivity {
+
+    private String[] items;
+    private ArrayList<String> listItems;
+    private ArrayList<String> databaseEvents;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<Event> data;
+    private ListView searchListView;
+    private EditText textsearch;
+    private String eventName;
+    private Event eventSelected;
 
 
+    protected void OnCreate (Bundle savedInstanceState){
 
-    //Eddie spaghetti or cowboy code here
-    private EditText editText;
-    //end of Eddie cowboy code
+        eventSelected = new Event();
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Firebase.setAndroidContext(this);
+        super.onCreate(savedInstanceState);
 
-        events = (ArrayList<Event>)getIntent().getSerializableExtra("allEvents");
 
-        /*
-        Eddie spa code
-         */
-        editText = (EditText) findViewById(R.id.textsearch);
-        editText.addTextChangedListener(new TextWatcher() {
+        databaseEvents = getIntent().getStringArrayListExtra("allEvents");
+        setContentView(R.layout.activity_search_event);
+
+        searchListView = (ListView) findViewById(R.id.searchListView);
+        textsearch = (EditText) findViewById(R.id.textsearch);
+        initList();
+
+        textsearch.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                if (s.toString().equals("")){
-                    return;
-                }
 
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().equals("")){
+                    //reset list view
+                    initList();
+                }
+                else {
+                    //do search
+                    searchItem(s.toString());
+                }
 
             }
 
@@ -63,101 +77,42 @@ public class SearchEvent extends Activity {
         });
 
 
+        //problem might be here -- > line 4 with setTitle or line 5 with EventActivity.class
+        searchListView.setOnItemClickListener (new AdapterView.OnItemClickListener(){
 
-        usr = (User) getIntent().getSerializableExtra("user");
-
-        database = new DBmanager();
-        setup_list();
-        setContentView(R.layout.activity_search_event);
-        elist = (ListView) findViewById(R.id.searchListView);
-        super.onCreate(savedInstanceState);
-
-        //create array list here
-      
-
-        //call getallevents from db
-        database.getAllEvents(usr);
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                eventName = (String) (parent.getItemAtPosition(position));
+                eventSelected.setTitle(eventName);
 
 
-        //iterate list to get event
-
-
-        /*
-        Eddie code
-         */
-        //get user data entry to search text field
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())){
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            DoSearch(query);
-        }
-        //end of search for event intent
-
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-        setTitle(usr.getUsername().toUpperCase() + " Events");
-
-        ArrayAdapter<Event> adapter = new EventAdapter();
-        elist.setAdapter(adapter);
+                Intent eventActivity = new Intent(view.getContext(), SearchEvent.class);
+                eventActivity.putExtra("selectedevent",  eventSelected);
+                startActivity(eventActivity);
+            }
+        });
 
 
 
     }
 
-    /*
-    Eddie code
-     */
-    //search for event method
-    public void DoSearch(String query) {
-        EditText ev = (EditText) findViewById(R.id.textsearch);
-
-        String evText = ev.getText().toString();
-        Event evt = new Event();
-        database.getEvent(usr, evt);
-        finish();
-    }
-    //end of Do Search Method
-
-
-
-    private void setup_list()
-    {
-        System.err.println(usr.getUsername());
-        events = database.getAllEvents(usr);
-
-    }
-
-
-
-
-    public class EventAdapter extends ArrayAdapter<Event> {
-        public EventAdapter() {
-            super(SearchEvent.this, R.layout.event_view, events);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View item = convertView;
-
-            if (item == null)
-                item = getLayoutInflater().inflate(R.layout.event_view, parent, false);
-
-            Event currentEvent = events.get(position);
-
-            TextView eventTitle = (TextView) item.findViewById(R.id.event_title);
-            eventTitle.setText(currentEvent.getTitle());
-
-            TextView eventDate = (TextView) item.findViewById(R.id.event_date);
-            eventDate.setText(currentEvent.getDate());
-
-            TextView eventTime = (TextView) item.findViewById(R.id.event_time);
-            eventTime.setText(currentEvent.getTime());
-            return item;
-
-
+    public void searchItem (String textToSearch){
+        textToSearch = textToSearch.substring(0, 1).toUpperCase() + textToSearch.substring(1);
+        for (String item: items){
+            if (!item.contains(textToSearch)){
+                listItems.remove(item);
+            }
 
         }
+        adapter.notifyDataSetChanged();
+    }
+
+    public void initList (){
+        items = new String[databaseEvents.size()];
+        items = databaseEvents.toArray(items);
+        listItems = new ArrayList<>(Arrays.asList(items));
+        adapter = new ArrayAdapter<>(this, R.layout.list_item, R.id.txtitem, listItems);
+        searchListView.setAdapter(adapter);
 
     }
 
